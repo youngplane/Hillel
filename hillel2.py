f26d8d972d17b637706eeb9acb964dea9d7710df
youngplane <greed.hoffmann@gmail.com>
2024-10-29 19:48:26 +0100

    final version

---
+++ hillel2.py
@@ -0,0 +1,15 @@
+strain = input('Enter your favourite Drink:')
+
+print('Your favourite Drink is:', strain)
+
+print(strain)
+print(type(strain))
+
+price = int(input('Enter your cash:'))
+print('Your favourite Drink is', strain, 'And he\'s worth:', price)
+
+print("Do you want something else?")
+
+
+
+

de4ea222659f09d7bfc4a84f90529c52319106f6
youngplane <greed.hoffmann@gmail.com>
2024-11-03 17:12:02 +0100

    list first reverse 0.0.02

--- listfirst.py
+++ listfirst.py
@@ -1,8 +1,8 @@
-def move_last_to_first(lst):
-    if len(lst) <= 1:
-        return lst
-    return [lst[-1]] + lst[:-1]
-
 your_list = [5, 1, 11, 6]
-result = move_last_to_first(your_list)
+
+if len(your_list) > 1:
+    result = [your_list[-1]] + your_list[:-1]
+else:
+    result = your_list
+
 print("Результат:", result)

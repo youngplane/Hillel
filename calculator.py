67ee35d8dd3ab61d5cffb4effc8ebe18e5a3a1e7
youngplane <greed.hoffmann@gmail.com>
2024-11-21 16:15:38 +0100

    calculator final version 2.0.0

--- calculator.py
+++ calculator.py
@@ -1,17 +1,26 @@
-#запрос
-number1 = float(input("Введите Ваше первое число: "))
-decision = input("Что вы хотите сделать? (+, -, *, /): ")
-number2 = float(input("Введите Ваше второе число: "))
+while True:
+    # Запрос_0
+    number1 = float(input("Введите Ваше первое число: "))
+    decision = input("Что вы хотите сделать? (+, -, *, /): ")
+    number2 = float(input("Введите Ваше второе число: "))
 
-#операции и чек на 0
-if decision == "+":
-    print("Ваш результат: ", number1 + number2)
-elif decision == "-":
-    print("Ваш результат: ", number1 - number2)
-elif decision == "*":
-    print("Ваш результат:", number1 * number2)
-elif decision == "/":
+    # Операции и чек на 0
+    if decision == "+":
+        print("Ваш результат: ", number1 + number2)
+    elif decision == "-":
+        print("Ваш результат: ", number1 - number2)
+    elif decision == "*":
+        print("Ваш результат:", number1 * number2)
+    elif decision == "/":
         if number2 != 0:
             print("Ваш результат: ", number1 / number2)
         else:
-            print("На ноль поделиться не получиться!")
+            print("На ноль поделиться не получится!")
+    else:
+        print("Неверная операция. Попробуйте снова.")
+
+    # Запрос_1
+    continue_calculator = input("Хотите продолжить? (y/yes для продолжения): ").lower()
+    if continue_calculator not in ("y", "yes"):
+        print("Работа калькулятора завершена. Спасибо!")
+        break

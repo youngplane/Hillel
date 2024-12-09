3d01ca33c5c1c5c4da5ed89331a40420cdf74e65
youngplane <greed.hoffmann@gmail.com>
2024-12-09 15:53:28 +0100

    vErSiOn 1.0000000

---
+++ generator_zif.py
@@ -0,0 +1,21 @@
+def some_gen(begin, end, func):
+    """
+    begin: перший елемент послідовності
+    end: кількість елементів у послідовності
+    func: функція, яка формує значення для послідовності
+    """
+    current = begin
+    for _ in range(end):
+        yield current
+        current = func(current)
+
+# Перевірка
+from inspect import isgenerator
+
+def pow(x):
+    return x ** 2
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
---
+++ pervoe_slovo.py
@@ -0,0 +1,13 @@
+def first_word(text):
+    import re
+    match = re.search(r"[a-zA-Z']+", text)
+    return match.group(0) if match else ""
+
+# Перевірка
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
